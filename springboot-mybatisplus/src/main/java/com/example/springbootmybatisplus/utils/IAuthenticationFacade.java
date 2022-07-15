32051ea0f76c5ee854cf60920432dc30183bebc8
叶子叶 <717818895@qq.com>
2022-07-15 12:57:21 +0800

    修改pom

--- springboot-mybatisplus/src/main/java/com/example/springbootmybatisplus/utils/IAuthenticationFacade.java
+++ springboot-mybatisplus/src/main/java/com/example/springbootmybatisplus/utils/IAuthenticationFacade.java
@@ -1,10 +1,12 @@
-package com.example.springbootmybatisplus.utils;
-
-import org.springframework.security.core.Authentication;
-
-/**
- * 获取用户
- */
-public interface IAuthenticationFacade {
-    Authentication getAuthentication();
-}
+//package com.example.springbootmybatisplus.utils;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 获取用户
+// */
+//@Component
+//public interface IAuthenticationFacade {
+//    Authentication getAuthentication();
+//}
