a3570217a19747b7b4b2fd8365a049edc0e37a06
叶子叶 <717818895@qq.com>
2022-07-18 18:18:06 +0800

    完成security适配

--- springboot-mybatisplus/src/main/java/com/example/springbootmybatisplus/mapper/RoleUserMapper.java
+++ springboot-mybatisplus/src/main/java/com/example/springbootmybatisplus/mapper/RoleUserMapper.java
@@ -2,6 +2,7 @@ package com.example.springbootmybatisplus.mapper;
 
 import com.example.springbootmybatisplus.entity.RoleUserEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author yzg
  * @since 2022-07-17
  */
+@Mapper
 public interface RoleUserMapper extends BaseMapper<RoleUserEntity> {
 
 }
