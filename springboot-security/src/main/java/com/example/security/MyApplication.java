1eb5605a2678bd873ea3d994fad0599d35a9a5c7
sun <393612177@qq.com>
2022-08-04 20:27:31 +0800

    pom整理

--- springboot-security/src/main/java/com/example/security/MyApplication.java
+++ springboot-security/src/main/java/com/example/security/MyApplication.java
@@ -4,12 +4,14 @@ package com.example.security;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 /* 如果configurationproperties的类上使用了component注解就不用
 @EnableConfigurationProperties({JsonWebTokenProperty.class})*/
-@SpringBootApplication()
+@SpringBootApplication
 @MapperScan("com.example.security.mapper")
 @EnableFeignClients
 public class MyApplication {
